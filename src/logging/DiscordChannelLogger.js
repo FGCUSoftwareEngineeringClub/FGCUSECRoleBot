210780bd5aff718bc3ffd1c9be6601bb0c467f82
tgayle <travisgayledev@gmail.com>
2018-10-26 19:07:22 -0400

    Fix Discord channel logging.

--- src/logging/DiscordChannelLogger.js
+++ src/logging/DiscordChannelLogger.js
@@ -1,6 +1,5 @@
 //@ts-check
 const BaseTransport = require('winston-transport');
-const winston = require('winston');
 const Discord = require('discord.js');
 const Settings = require('../settings');
 const logger = require('./Logger');
@@ -9,37 +8,33 @@ const logger = require('./Logger');
  * This is a custom transporter from the winston library to allow logging messages to a discord
  * server channel.
  * @extends BaseTransport
- * @property {Discord.Client} this.discordClient
  */
 class DiscordChannelTransport extends BaseTransport {
   /**
    * 
-   * @param {winston.Transport.TransportStreamOptions} options 
-   * @param {Discord.Client} options.client
+   * @param {{client: Discord.Client} & BaseTransport.TransportStreamOptions} options 
    */
   constructor(options) {
     super(options);
+    /**
+     * @type {Discord.Client}
+     * @private
+     */
     this.discordClient = options.client;
 
-    // /** Sets a boolean for whether or not any logging text channel is set in settings. */
-    // this.loggingChannelExists = Settings.LOGGING_TEXT_CHANNEL ? true : false;
-    // if (this.loggingChannelExists) {
-    //   const primaryServer = this.discordClient.guilds.get(Settings.PRIMARY_SERVER_ID);
-    //   this.loggingChannel = primaryServer.channels.get(Settings.LOGGING_TEXT_CHANNEL);
-    // }
-    /**
-     * @typedef {Map<string, Discord.TextChannel>}
-     */
-    this.channels = new Map();
-    options.client.guilds.forEach(function (server) {
-      this.channels.set(server.id, server.channels.filter(channel => channel.name.includes('bot') && channel.name.includes('log')).first());
-    });
+    /** Sets a boolean for whether or not any logging text channel is set in settings. */
+    this.loggingChannelExists = Settings.LOGGING_TEXT_CHANNEL ? true : false;
+    if (this.loggingChannelExists) {
+      const primaryServer = this.discordClient.guilds.get(Settings.PRIMARY_SERVER_ID);
+
+      this.loggingChannel = primaryServer.channels.get(Settings.LOGGING_TEXT_CHANNEL);
+    }
 
     logger.info('Discord Channel Logger Active!');
   }
 
   async log(message, onFinished) {
-    // if (this.loggingChannelExists) {
+    if (this.loggingChannelExists) {
       /**
        * If the message argument is an object with a message property, message will be set to that,
        * otherwise it will be set to the message itself.
@@ -48,13 +43,8 @@ class DiscordChannelTransport extends BaseTransport {
 
       // If this message spans multiple lines, place it in a codeblock to try preserving formatting.
       // await this.loggingChannel.send(messageText, {code: messageText.includes('\n')});
-      const logs = [];
-      const logMessagesPromiseArray = this.channels.forEach(function (channel) {
-        logs.push(channel.send(messageText, {code: messageText.includes('\n')}));
-      })
-
-      await Promise.all(logs);
-    // }
+      this.loggingChannel.send(messageText, {code: messageText.includes('\n')});
+    }
     
     /*
      If a callback was passed in, then call it. Useful if you want to do something after a message
